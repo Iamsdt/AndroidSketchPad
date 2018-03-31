a9f9dda50a760bdc00f3503612f80c5ce488a7fa
Shudipto <Shudiptotrafder@gmail.com>
2018-03-31 11:03:51 +0600

    Initial commit

---
+++ app/src/main/java/com/iamsdt/androidsketchpad/MyApplication.kt
@@ -0,0 +1,14 @@
+package com.iamsdt.androidsketchpad
+
+import android.app.Application
+
+/**
+ * Created by Shudipto Trafder on 3/31/2018.
+ * at 10:59 AM
+ */
+class MyApplication:Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
