99ec5b1f68dcb0fb07ce88b7ec1cf0d9d5d2891f
Iamsdt <shudiptotrafder@gmail.com>
2018-06-09 18:19:25 +0600

    test call

--- app/src/main/java/com/iamsdt/androidsketchpad/ui/MainActivity.kt
+++ app/src/main/java/com/iamsdt/androidsketchpad/ui/MainActivity.kt
@@ -2,9 +2,14 @@ package com.iamsdt.androidsketchpad.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.iamsdt.androidsketchpad.BuildConfig
 import com.iamsdt.androidsketchpad.R
 import com.iamsdt.androidsketchpad.data.retrofit.RetInterface
-import com.squareup.picasso.Picasso
+import com.iamsdt.androidsketchpad.data.retrofit.model.posts.PostsResponse
+import kotlinx.coroutines.experimental.async
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import timber.log.Timber
 import javax.inject.Inject
@@ -12,19 +17,49 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject lateinit var retrofit: Retrofit
-
-    @Inject lateinit var picasso: Picasso
-
     @Inject
     lateinit var retInterface: RetInterface
+
+    @Inject
+    lateinit var retrofit: Retrofit
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val url = retrofit.baseUrl()
 
-        Timber.i("$retrofit and picasso$picasso")
+        getData()
 
+    }
+
+    private fun getData() {
+        async {
+            retInterface.getPostForFirstTime(BuildConfig.BloggerApiKey)
+                    .enqueue(object :Callback<PostsResponse>{
+                        override fun onFailure(call: Call<PostsResponse>?, t: Throwable?) {
+                            Timber.e(t,"data load failed")
+                        }
+
+                        override fun onResponse(call: Call<PostsResponse>?,
+                                                response: Response<PostsResponse>?) {
+
+                            Timber.i("response:$response")
+
+                            if (response != null && response.isSuccessful){
+                                val post =  response.body()
+
+
+                                    Timber.i("Page Token: ${post?.nextPageToken}")
+
+                                    val itemsItem = post?.items ?: emptyList()
+                                    for (item in itemsItem){
+                                        Timber.i("Post title: ${item.title}")
+                                    }
+                                }
+                            }
+
+                    })
+        }
     }
 }
--- app/src/main/java/com/iamsdt/androidsketchpad/ui/main/MainVM.kt
+++ app/src/main/java/com/iamsdt/androidsketchpad/ui/main/MainVM.kt
@@ -7,6 +7,6 @@ package com.iamsdt.androidsketchpad.ui.main
 
 import android.arch.lifecycle.ViewModel
 
-class MainVM:ViewModel(){
+class MainVM constructor():ViewModel(){
 
 }
