a8e7409208bbe94c2bfbb19a944e7f139e547e1e
Iamsdt <shudiptotrafder@gmail.com>
2018-06-10 00:15:40 +0600

    data layer inject

--- app/src/main/java/com/iamsdt/androidsketchpad/injection/module/AppModule.kt
+++ app/src/main/java/com/iamsdt/androidsketchpad/injection/module/AppModule.kt
@@ -6,17 +6,36 @@
 package com.iamsdt.androidsketchpad.injection.module
 
 import android.app.Application
+import com.iamsdt.androidsketchpad.BuildConfig
+import com.iamsdt.androidsketchpad.data.loader.LayerUtils
+import com.iamsdt.androidsketchpad.data.loader.RemoteDataLayer
+import com.iamsdt.androidsketchpad.data.retrofit.RetInterface
+import com.iamsdt.androidsketchpad.database.dao.PageTableDao
+import com.iamsdt.androidsketchpad.database.dao.PostTableDao
 import com.iamsdt.androidsketchpad.utils.SpUtils
 import dagger.Module
 import dagger.Provides
-import dagger.android.DaggerApplication
 import javax.inject.Singleton
 
 @Module
-class AppModule{
+class AppModule {
 
     @Provides
     @Singleton
-    fun getSp(application: Application):SpUtils
-     = SpUtils(application)
+    fun getDataLayer(spUtils: SpUtils,
+                     retInterface: RetInterface,
+                     layerUtils: LayerUtils):RemoteDataLayer =
+            RemoteDataLayer(spUtils,retInterface,layerUtils,BuildConfig.BloggerApiKey)
+
+
+    @Provides
+    @Singleton
+    fun getLayerUtils(spUtils: SpUtils,
+                      pageTableDao: PageTableDao,
+                      postTableDao: PostTableDao): LayerUtils =
+            LayerUtils(spUtils,pageTableDao,postTableDao)
+
+    @Provides
+    @Singleton
+    fun getSp(application: Application): SpUtils = SpUtils(application)
 }
