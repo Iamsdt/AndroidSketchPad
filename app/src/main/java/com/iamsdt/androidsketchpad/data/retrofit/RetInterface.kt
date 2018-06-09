9656861a91404516f924f62cb726ef3163a54a42
Iamsdt <shudiptotrafder@gmail.com>
2018-06-09 14:21:08 +0600

    rest interface finished

--- app/src/main/java/com/iamsdt/androidsketchpad/data/retrofit/RetInterface.kt
+++ app/src/main/java/com/iamsdt/androidsketchpad/data/retrofit/RetInterface.kt
@@ -1,6 +1,11 @@
 package com.iamsdt.androidsketchpad.data.retrofit
 
-import com.iamsdt.androidsketchpad.BuildConfig
+import com.iamsdt.androidsketchpad.data.retrofit.model.blog.BlogResponse
+import com.iamsdt.androidsketchpad.data.retrofit.model.page.PageResponse
+import com.iamsdt.androidsketchpad.data.retrofit.model.posts.PostsResponse
+import com.iamsdt.androidsketchpad.data.retrofit.model.singlePage.SinglePageResponse
+import com.iamsdt.androidsketchpad.data.retrofit.model.singlePost.SinglePostResponse
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -12,33 +17,35 @@ import retrofit2.http.Query
 interface RetInterface {
 
     @GET("/posts?fetchImages=true")
-    fun getPostForFirstTime(@Query("key") key: String)
+    fun getPostForFirstTime(@Query("key") key: String):Call<List<PostsResponse>>
 
     //posts/search?q=documentation
     @GET("/posts?fetchImages=true")
     fun getPostFormSearch(
             @Query("search") search: String,
-            @Query("key") key: String)
+            @Query("key") key: String):Call<List<PostsResponse>>
 
     @GET("/posts?fetchImages=true")
     fun getPostFormSearchWithToken(@Query("getPostWithToken") token: String,
-                                   @Query("key") key: String)
+                                   @Query("key") key: String):Call<List<PostsResponse>>
 
     //posts/postId
     @GET("/posts/{id}?fetchImages=true")
-    fun getSinglePost(@Path("id") posID: Int, @Query("key") key: String)
+    fun getSinglePost(@Path("id") posID: Int,
+                      @Query("key") key: String):Call<SinglePostResponse>
 
     ///pages?fetchImages=true&key
     @GET("/pages/?fetchImages=true")
-    fun getPageList(@Query("key") key: String)
+    fun getPageList(@Query("key") key: String):Call<List<PageResponse>>
 
     @GET("/pages/{id}?fetchImages=true")
-    fun getSinglePage(@Path("id") posID: Int, @Query("key") key: String)
+    fun getSinglePage(@Path("id") posID: Int,
+                      @Query("key") key: String):Call<SinglePageResponse>
 
     @GET("/pageviews?range=all")
     fun getPageView(@Query("key") key: String)
 
 
     @GET
-    fun getBlog()
+    fun getBlog():Call<BlogResponse>
 }
--- app/src/main/java/com/iamsdt/androidsketchpad/data/test.kt
+++
@@ -1,6 +0,0 @@
-package com.iamsdt.androidsketchpad.data
-
-/**
- * Created by Shudipto Trafder on 3/31/2018.
- * at 11:41 AM
- */
