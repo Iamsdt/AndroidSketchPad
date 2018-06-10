c6dcac797153b6994fa2ce968156e282a72df7f5
Iamsdt <shudiptotrafder@gmail.com>
2018-06-10 16:27:37 +0600

    adding main activity

--- app/src/main/java/com/iamsdt/androidsketchpad/ui/SplashScreen.kt
+++ app/src/main/java/com/iamsdt/androidsketchpad/ui/SplashScreen.kt
@@ -1,12 +1,20 @@
 package com.iamsdt.androidsketchpad.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.iamsdt.androidsketchpad.R
+import com.iamsdt.androidsketchpad.data.loader.DataLayer
+import com.iamsdt.androidsketchpad.ui.main.MainActivity
+import com.iamsdt.androidsketchpad.ui.services.UpdateService
+import com.iamsdt.androidsketchpad.utils.ConnectivityChangeReceiver
 import com.iamsdt.androidsketchpad.utils.ConstantUtils.Companion.connected
 import com.iamsdt.androidsketchpad.utils.ConstantUtils.Companion.internet
+import com.iamsdt.androidsketchpad.utils.ConstantUtils.Event.SERVICE
+import com.iamsdt.androidsketchpad.utils.SpUtils
 import com.iamsdt.androidsketchpad.utils.ext.ToastType
 import com.iamsdt.androidsketchpad.utils.ext.showToast
+import com.iamsdt.androidsketchpad.utils.ext.toNextActivity
 import com.iamsdt.androidsketchpad.utils.model.EventMessage
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
@@ -18,10 +26,37 @@ class SplashScreen : AppCompatActivity() {
     @Inject
     lateinit var bus: EventBus
 
+    @Inject
+    lateinit var spUtils: SpUtils
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
+
+        if (spUtils.isFirstTime){
+            //show app intro
+            //runThread(500,)
+            toNextActivity(MainActivity::class)
+        } else{
+            //show main screen
+            //runThread(1000,)
+        }
+
+
+        //runServices()
     }
+
+    private fun runServices(){
+        if (ConnectivityChangeReceiver.getInternetStatus(this)
+                && timeForUpdate()){
+            startService(Intent(this,UpdateService::class.java))
+        }
+    }
+
+    private fun timeForUpdate():Boolean{
+        return false
+    }
+
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     fun busEvent(eventMessage: EventMessage){
@@ -32,8 +67,13 @@ class SplashScreen : AppCompatActivity() {
                 showToast(ToastType.WARNING,"No Internet")
             }
         }
-    }
 
+        if (eventMessage.key == SERVICE && eventMessage.status == 0){
+            if (UpdateService.isRunningService){
+                stopService(Intent(this,UpdateService::class.java))
+            }
+        }
+    }
 
     override fun onStart() {
         super.onStart()
--- app/src/main/java/com/iamsdt/androidsketchpad/ui/services/UpdateService.kt
+++ app/src/main/java/com/iamsdt/androidsketchpad/ui/services/UpdateService.kt
@@ -9,6 +9,13 @@ import android.arch.lifecycle.LifecycleService
 import android.arch.lifecycle.Observer
 import android.content.Intent
 import com.iamsdt.androidsketchpad.data.loader.RemoteDataLayer
+import com.iamsdt.androidsketchpad.utils.ConnectivityChangeReceiver
+import com.iamsdt.androidsketchpad.utils.ConstantUtils.Event.BLOG_KEY
+import com.iamsdt.androidsketchpad.utils.ConstantUtils.Event.PAGE_KEY
+import com.iamsdt.androidsketchpad.utils.ConstantUtils.Event.POST_KEY
+import com.iamsdt.androidsketchpad.utils.DateUtils
+import com.iamsdt.androidsketchpad.utils.SpUtils
+import com.iamsdt.androidsketchpad.utils.model.EventMessage
 import dagger.android.AndroidInjection
 import org.greenrobot.eventbus.EventBus
 import timber.log.Timber
@@ -22,26 +29,110 @@ class UpdateService : LifecycleService() {
     @Inject
     lateinit var bus: EventBus
 
+    @Inject
+    lateinit var spUtils: SpUtils
+
+    private var postFinished = false
+    private var pageFinished = false
+    private var blogFinished = false
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         Timber.i("Update Service start")
 
-        remoteDataLayer.getBlogDetails()
-        remoteDataLayer.getPageDetails()
-        remoteDataLayer.getPostDetailsForFirstTime()
+        if (spUtils.isServiceFirstTime) {
+            remoteDataLayer.getPostDetailsForFirstTime()
+            remoteDataLayer.getPageDetails()
+            remoteDataLayer.getBlogDetails()
 
-        remoteDataLayer.getPostWithToken()
+        } else {
+            remoteDataLayer.getBlogDetails()
+            if (timeForUpdate()) {
+                remoteDataLayer.getPostDetailsForFirstTime()
+                remoteDataLayer.getPageDetails()
+            }
+        }
 
         remoteDataLayer.layerUtils
                 .serviceLiveData.observe(this, Observer {
-            // TODO: 6/9/2018 add logic
+            if (it != null) {
+                when (it.key) {
+                    POST_KEY -> {
+                        if (it.status == 1) {
+                            postFinished = true
+                            if (remoteDataLayer.isReadyForNextToken()) {
+                                remoteDataLayer.getPostWithToken(spUtils.getPageToken)
+                            }
+                        } else {
+                            if (it.message.isEmpty()){
+                                if (isNet()) remoteDataLayer.getPostDetailsForFirstTime()
+                            } else{
+                                if (isNet()) remoteDataLayer.getPostWithToken(spUtils.getPageToken)
+                            }
+                        }
+                    }
+                    PAGE_KEY -> {
+                        if (it.status == 1) {
+                            pageFinished = true
+                        } else {
+                            if (isNet()) remoteDataLayer.getPageDetails()
+                        }
+                    }
+                    BLOG_KEY -> {
+                        if (it.status == 1) {
+                            blogFinished = true
+                            checkForOtherUpdate()
+                        } else {
+                            if (isNet()) remoteDataLayer.getBlogDetails()
+                        }
+                    }
+                }
+
+                setUpdateTime()
+            }
         })
 
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private fun setUpdateTime() {
+        if (blogFinished && pageFinished && postFinished) {
+            spUtils.setServiceRunDate()
+            bus.post(EventMessage())
+        }
+    }
+
+    private fun timeForUpdate(): Boolean {
+        val date = spUtils.getServiceRunDate
+        if (date == 0L) return true
+        val interval = DateUtils.compareDateIntervals(date)
+        val savedInterVal = spUtils.syncTimeInterval(this)
+
+        return interval >= savedInterVal
+    }
+
+
+    private fun isNet() = ConnectivityChangeReceiver.getInternetStatus(this)
+
+    private fun checkForOtherUpdate() {
+        if (!timeForUpdate()) {
+            postFinished = true
+            blogFinished = true
+        }
+    }
+
     override fun onCreate() {
         AndroidInjection.inject(this)
         super.onCreate()
+        isRunningService = true
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunningService = false
+    }
+
+    companion object {
+        var isRunningService = false
     }
 }
